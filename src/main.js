b3aacbaba306890c8015d3c0019b6cc9eec25e0c
simao234430 <simao234430@gmail.com>
2021-04-05 17:48:06 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import VueRouter from 'vue-router'
+import Antd from 'ant-design-vue/es'
 
+Vue.use(Antd);
+Vue.use(VueRouter);
 Vue.config.productionTip = false
+
+import 'ant-design-vue/dist/antd.less';  // or 'ant-design-vue/dist/antd.less'
+
 
 new Vue({
   render: h => h(App),
+  router,
+  store,
 }).$mount('#app')
